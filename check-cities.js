c3932112a5fbc39ae5db42315d3f3741f9ed4903
John Owolabi <johnowolabi80@gmail.com>
2025-10-10 11:10:09 -0400

    🧹 Clean up temporary files

--- check-cities.js
+++
@@ -1,6 +0,0 @@
-import { getAllCities } from './config/regions.js';
-
-const cities = getAllCities();
-console.log('Total cities:', cities.length);
-console.log('Cities:');
-cities.forEach((city, i) => console.log(`${i+1}. ${city.name} (${city.regionName})`));
